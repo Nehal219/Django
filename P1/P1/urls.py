c5e8381f22ca74564e0be3646525cee41631b890
Nehal219 <mahmadnehal502@gmail.com>
2025-12-29 10:26:24 +0530

    Basic Output

--- P1/P1/urls.py
+++ P1/P1/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('',m1)
+    path('nehal/',m1),
+    path('nehalimg/',m2)
 ]
